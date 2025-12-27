ea67508b9be49de87ab82ee8020f4c2712b2c4e1
saierukala <sai.erukala1508@gamil.com>
2025-12-27 23:08:40 +0530

    database connection

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -18,8 +19,10 @@ app.get("/", (req, res) => {
   res.send("welcome to the subscription tracker api!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
--- config/env.js
+++ config/env.js
@@ -4,5 +4,10 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const PORT = process.env.PORT || 5500;
 export const NODE_ENV = process.env.NODE_ENV || "development";
+export const PORT = Number(process.env.PORT) || 5500;
+export const DB_URI = process.env.DB_URI;
+
+if (!DB_URI) {
+  throw new Error("❌ DB_URI is not defined in environment variables");
+}
